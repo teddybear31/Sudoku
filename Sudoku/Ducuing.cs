a4d5effa42313b449c02288ec39a8a9f4b0fb5a4
Hiku <Hikuku@hotmail.fr>
2022-01-25 11:47:46 +0100

    Ajout de mon texte dans la méthode Ducuing

--- Sudoku/Ducuing.cs
+++ Sudoku/Ducuing.cs
@@ -7,6 +7,6 @@ public class Ducuing
 
 	public void MaMethode()
     {
-		Console.WriteLine("Ceci est la méthode de Ducuing");
+		Console.WriteLine("Ceci est la méthode de Ducuing, ajout du texte venant de Viatgé");
     }
 }
