a22298832c438918b0aebbb7eaf9aaf360cbf0fa
zork0w <zertroxtv@gmail.com>
2022-01-28 15:06:15 +0100

    Creation de la classe Bieda.cs contenant la méthode.

---
+++ Sudoku/Bieda.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sudoku
+{
+    public class Bieda
+    {
+        public void ElMethodo()
+        {
+            Console.WriteLine("Ce fichier est meilleur que tous les autres, soyez pas jaloux (j'ai oublié le point virgule)");
+        }
+    }
+
+
+}
